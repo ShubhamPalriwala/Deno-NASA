2d6c3166d05971a2b9a80fdd8d302998821bdf3b
LaalShaitaan <spalriwalau@gmail.com>
2020-06-13 16:37:52 +0530

    Filtered Data using Lodash.js

--- mod.ts
+++ mod.ts
@@ -2,6 +2,8 @@ import {join} from "https://deno.land/std/path/mod.ts";
 import {BufReader} from "https://deno.land/std/io/bufio.ts";
 import {parse} from "https://deno.land/std/encoding/csv.ts";
 
+import * as _ from "https://raw.githubusercontent.com/lodash/lodash/4.17.15-es/lodash.js";
+
 interface Planet{
     [key:string]: string
 }
@@ -24,8 +26,18 @@ async function loadPlanetData() {
         return((planet["koi_disposition"]==="CONFIRMED") && (planetaryRadius>0.5 && planetaryRadius<1.5) && (stellarRadius>0.99 && stellarRadius<1.01));
     })
 
-    return planets;
+    return planets.map((planet)=>{
+        return _.pick(planet,[
+            "koi_prad",
+            "koi_srad",
+            "kepler_name",
+            "koi_steff"
+        ])
+    });
 }
 
 const newEarths=await loadPlanetData()
+for(const planet of newEarths){
+    console.log(planet)
+}
 console.log(`${newEarths.length} habitable planets found.`)
