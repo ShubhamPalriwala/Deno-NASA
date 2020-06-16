734f191c9d536e230bbe7eb7197fbbc8573b37c5
LaalShaitaan <spalriwalau@gmail.com>
2020-06-16 17:26:44 +0530

    Pushing FrontEnd

--- mod.ts
+++ mod.ts
@@ -1,43 +1,29 @@
-import {join} from "https://deno.land/std/path/mod.ts";
-import {BufReader} from "https://deno.land/std/io/bufio.ts";
-import {parse} from "https://deno.land/std/encoding/csv.ts";
+import { Application } from "https://deno.land/x/oak@v5.0.0/mod.ts";
 
-import * as _ from "https://raw.githubusercontent.com/lodash/lodash/4.17.15-es/lodash.js";
+const app=new Application();
+const PORT=3000
 
-interface Planet{
-    [key:string]: string
-}
+app.use(async(ctx,next)=>{
+    await next();
+    const time=ctx.response.headers.get("X-Response-Time");
+    console.log(`${ctx.request.method} ${ctx.request.url}: ${time}`)
+})
 
-async function loadPlanetData() {
-    const path=join(".","cumulative_2020.06.13_03.21.06.csv");
-    const file=await Deno.open(path);
-    const bufReader=new BufReader(file);
+app.use(async(ctx,next)=>{
+    const start=Date.now()
+    await next();
+    const delta=Date.now()-start;
+    ctx.response.headers.set("X-Response-Time",`${delta}ms`);
 
-    const result = await parse(bufReader,{
-        header:true,
-        comment:"#"
+})
+
+app.use((ctx)=>{
+    ctx.response.body="Hello World";
+});
+
+if(import.meta.main)
+{
+    await app.listen({
+        port:PORT
     })
-    Deno.close(file.rid);
-
-    const planets=(result as Array<Planet>).filter((planet)=>{
-        const planetaryRadius=Number(planet["koi_prad"]);
-        const stellarRadius=Number(planet["koi_srad"]);
-
-        return((planet["koi_disposition"]==="CONFIRMED") && (planetaryRadius>0.5 && planetaryRadius<1.5) && (stellarRadius>0.99 && stellarRadius<1.01));
-    })
-
-    return planets.map((planet)=>{
-        return _.pick(planet,[
-            "koi_prad",
-            "koi_srad",
-            "kepler_name",
-            "koi_steff"
-        ])
-    });
 }
-
-const newEarths=await loadPlanetData()
-for(const planet of newEarths){
-    console.log(planet)
-}
-console.log(`${newEarths.length} habitable planets found.`)
